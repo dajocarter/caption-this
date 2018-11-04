a5f3c6934d96716a07094d47eb5d435a67944ea0
David Carter <dajocarter@gmail.com>
2018-11-04 11:11:01 -0800

    Update the vote api call

--- functions/gifs-update.js
+++ functions/gifs-update.js
@@ -10,11 +10,15 @@ exports.handler = (event, context, callback) => {
   const id = getId(event.path);
   console.log(`Function 'gif-update' invoked. update id: ${id}`);
   return client
-    .query(q.Let({ gif: q.Get(q.Ref(`classes/gifs/${id}`)) }, q.Var("gif")))
-    .then(response =>
-      q.Update(q.Ref(`classes/gifs/${id}`), {
-        data: { votes: response.data.gifId + 1 }
-      })
+    .query(
+      q.Let(
+        { gif: q.Get(q.Ref(`classes/gifs/${id}`)) },
+        q.Lambda(
+          q.Update(q.Ref(`classes/gifs/${id}`), {
+            data: { votes: q.Select(["data", "votes"], q.Var("gif")) + 1 }
+          })
+        )
+      )
     )
     .then(response => {
       console.log("success", response);
