09cb6614fb179be3d627062858548c601c0abede
David Carter <dajocarter@gmail.com>
2018-11-04 17:30:14 -0800

    swap api key

--- src/components/GifCaption/index.js
+++ src/components/GifCaption/index.js
@@ -15,7 +15,7 @@ export default class GifCaption extends Component {
 
   getRandomGif() {
     return fetch(
-      `https://api.giphy.com/v1/gifs/random?api_key=tbPlgH8qoKweZyZ0gQaXN21HR5NM6YDi&rating=pg-13`
+      `https://api.giphy.com/v1/gifs/random?api_key=n0qlHVkcdxtfyqU1SPbAL9lt3yxdOR0k&rating=pg-13`
     )
       .then(resp => resp.json())
       .then(response => {
