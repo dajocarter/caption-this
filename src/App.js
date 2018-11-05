bbe28e3cd004381f1c96f31d0d86d8cea4e5cf7a
David Carter <dajocarter@gmail.com>
2018-11-04 17:11:49 -0800

    Add faunadb to App component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,35 @@ import AppHeader from "./components/AppHeader";
 import GifCaption from "./components/GifCaption";
 import "./App.css";
 import GifsList from "./components/GifsList";
+import Login from "./components/Login";
+import faunadb from "faunadb";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { faunadb_token: null, error: null };
+  }
+
+  onAuthChange(faunadb_token) {
+    console.log("app.js onAuthChange", faunadb_token);
+    this.setState({ faunadb_token });
+    this.client = new faunadb.Client({ secret: faunadb_token });
+    console.log("onAuthChange", faunadb_token);
+  }
+
+  onError(error) {
+    this.setState({ error });
+  }
+
   render() {
     return (
       <div className="app">
         <AppHeader />
+        <Login
+          onError={this.onError.bind(this)}
+          onAuthChange={this.onAuthChange.bind(this)}
+        />
         <div className="container">
           <GifCaption />
           <GifsList />
--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Login from "../Login";
 
 const AppHeader = props => {
   return (
@@ -8,7 +7,6 @@ const AppHeader = props => {
         <div className="app-left-nav">
           <div className="app-title-text">
             <h1 className="app-title">CaptionThis!</h1>
-            <Login />
           </div>
         </div>
       </div>
